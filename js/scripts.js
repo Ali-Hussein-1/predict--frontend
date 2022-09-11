0a7192bbb99fe8ecf0da4177e3fc7eaf26ead852
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-09-11 09:45:57 +0300

    Adding some comments

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,7 @@ var test
 
 
 fetch("https://dog.ceo/api/breeds/image/random")
-  .then(function(response){
-      console.log(response)
-      return response.json()
-  })
+.then((response) => response.json())
 .then(function(data){display(data.message)})
 function display(image_url){
     document.getElementById("image").src=image_url
@@ -39,7 +36,7 @@ function getNat(){
   fetch('https://api.nationalize.io/?name=' + test)
   .then((response) => response.json())
   .then((data)=>console.log(data.country))
-  .then(function(data){
+  .then((data) => {
     let country= data.country
     let nat_id = country[0].country_id
     if (country.length==1){
@@ -56,16 +53,16 @@ function getNat(){
 
 
 
-async function getNationality(){
-    const response = await fetch(' https://api.nationalize.io/?name=' + text)
-    const data= await response.json()
-    let country= data.country
-    let nat_id = country[0].country_id
-    if (country.length==1){
-        nat_id=country[0].country_id
-    }
-    else{
-        nat_id = country[0].country_id + "and his second nationality is " + country[1].country_id
-    }
-    console.log(nat_id)
-  }
+// async function getNationality(){
+//     const response = await fetch(' https://api.nationalize.io/?name=' + text)
+//     const data= await response.json()
+//     let country= data.country
+//     let nat_id = country[0].country_id
+//     if (country.length==1){
+//         nat_id=country[0].country_id
+//     }
+//     else{
+//         nat_id = country[0].country_id + "and his second nationality is " + country[1].country_id
+//     }
+//     console.log(nat_id)
+//   }
