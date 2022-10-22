8efe1e1cd9469a42eed76b8f970d5e2bec29efed
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-10-22 11:33:08 +0300

    Declaring URL's

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,51 @@
 // Some sections where commented due to bug issues
-const btn = document.querySelector("#btn")
-const para = document.querySelector("#para")
-var test
+const btn = document.querySelector("#btn");
+const para = document.querySelector("#para");
+var test;
+const img_url = "https://dog.ceo/api/breeds/image/random";
+const gender_url = "https://api.genderize.io?name=";
+const age_url = "https://api.agify.io/?name=";
+const nat_url = "https://api.nationalize.io/?name=";
 
 // displaying the image
 fetch("https://dog.ceo/api/breeds/image/random")
-.then((response) => response.json())
-.then(function(data){display(data.message)})
-function display(image_url){
-    document.getElementById("image").src=image_url
+  .then((response) => response.json())
+  .then(function (data) {
+    display(data.message);
+  });
+function display(image_url) {
+  document.getElementById("image").src = image_url;
 }
 
 // click event on the button to get age name and gender
-btn.addEventListener("click",get_name)
-function get_name(){
-    test = document.getElementById("test").value
-    getAge()
-    getGender()
-    getNat()
+btn.addEventListener("click", get_name);
+function get_name() {
+  test = document.getElementById("test").value;
+  getAge();
+  getGender();
+  getNat();
 }
 // getting the gender
-function getGender(){
-  fetch('https://api.genderize.io?name=' + test)
-  .then((response) => response.json())
-  .then((data)=>console.log(data.gender))
+function getGender() {
+  fetch("https://api.genderize.io?name=" + test)
+    .then((response) => response.json())
+    .then((data) => console.log(data.gender));
 }
 // getting the age
-function getAge(){
-  fetch('https://api.agify.io/?name=' + test)
-  .then((response) => response.json())
-  .then((data)=>console.log(data.age))
+function getAge() {
+  fetch("https://api.agify.io/?name=" + test)
+    .then((response) => response.json())
+    .then((data) => console.log(data.age));
   // .then(function(){
   //       para.innerHTML = data.age
   //       })
 }
 // getting the nationality
-function getNat(){
-  fetch('https://api.nationalize.io/?name=' + test)
-  .then((response) => response.json())
-  .then((data)=>console.log(data.country))}
+function getNat() {
+  fetch("https://api.nationalize.io/?name=" + test)
+    .then((response) => response.json())
+    .then((data) => console.log(data.country));
+}
 //   .then((data) => {
 //     let country= data.country
 //     let nat_id = country[0].country_id
